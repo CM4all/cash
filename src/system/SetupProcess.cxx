39346977fb4b29a7fdd0e4b744105b79ea60c9f9
Max Kellermann <max.kellermann@ionos.com>
2025-08-04 21:33:30 +0200

    update my email address

--- src/system/SetupProcess.cxx
+++ src/system/SetupProcess.cxx
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause OR GPL-2.0-or-later
 // Copyright CM4all GmbH
-// author: Max Kellermann <mk@cm4all.com>
+// author: Max Kellermann <max.kellermann@ionos.com>
 
 #include "SetupProcess.hxx"
 
